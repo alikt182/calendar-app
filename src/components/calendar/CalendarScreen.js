0a153a84955c5baf4c6097ac574cbd2f95d5a0ae
Ali Monserrate <ali.monserrate.icg@gmail.com>
2022-01-28 16:22:23 -0400

    Creación de Login Screen, Navbar. Instalacion de BigCalendar Diseño de calendario

--- src/components/calendar/CalendarScreen.js
+++ src/components/calendar/CalendarScreen.js
@@ -1,9 +1,56 @@
-import React from 'react';
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import moment from 'moment';
+
+import { Navbar } from '../ui/Navbar';
+import { messages } from '../../helpers/calendar-messages';
+
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+import 'moment/locale/es';
+
+moment.locale('es')
+
+const localizer = momentLocalizer(moment);
+
+const events = [{
+  title: 'Cumpleaños de Elver',
+  start: moment().toDate(),
+  end: moment().add( 2 , 'hours' ).toDate(),
+  bgcolor: '#fafafa',
+  notes: 'Comprar el pastel'
+}]
 
 export const CalendarScreen = () => {
+
+  const eventStyleGetter = ( event, start, end, isSelected ) => {
+    //console.log(event, start, end, isSelected)
+
+    const style = {
+      backgroundColor: '#367CF7',
+      borderRadius: '0px',
+      opacity: 0.8,
+      display: 'block',
+      color: 'white'
+    }
+
+
+    return {
+      style
+    }
+  }
+
   return (
-      <div>
-          <h1>Calendar Screen</h1>
+      <div className="calendar-screen">
+              <Navbar/>
+              <Calendar
+                localizer={localizer}
+                events={ events }
+                startAccessor="start"
+                endAccessor="end"
+                //style={{ height: 500 }}
+                messages={ messages }
+                eventPropGetter={ eventStyleGetter }
+              />
+
       </div>
   )
 };
