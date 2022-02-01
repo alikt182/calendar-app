a34761b075c3306e0a718161db6a3736450dc8dc
Ali Monserrate <ali.monserrate.icg@gmail.com>
2022-02-01 16:36:01 -0400

    Configuracion de Modal, validacion del formulario, datePicker, instalación y configuracion de Redux

--- src/components/calendar/CalendarScreen.js
+++ src/components/calendar/CalendarScreen.js
@@ -1,8 +1,13 @@
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
 import { Calendar, momentLocalizer } from 'react-big-calendar';
 import moment from 'moment';
 
 import { Navbar } from '../ui/Navbar';
 import { messages } from '../../helpers/calendar-messages';
+import { CalendarEvent } from './CalendarEvent';
+import { CalendarModal } from './CalendarModal';
+import { uiOpenModal } from '../actions/ui';
 
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 import 'moment/locale/es';
@@ -16,10 +21,32 @@ const events = [{
   start: moment().toDate(),
   end: moment().add( 2 , 'hours' ).toDate(),
   bgcolor: '#fafafa',
-  notes: 'Comprar el pastel'
+  notes: 'Comprar el pastel',
+  user: {
+    _id: '123',
+    name: 'Andrea'
+  }
 }]
 
 export const CalendarScreen = () => {
+
+  const dispatch = useDispatch();
+
+  const [lastView, setLastView] = useState( localStorage.getItem('lastView') || 'month' );
+
+  const onDoubleClick = (e) => {
+    //console.log(e);
+    dispatch( uiOpenModal() );
+  }
+
+  const onSelectedEvent = (e) => {
+    console.log(e);
+  }
+
+  const onViewChange = (e) => {
+    setLastView(e); 
+    localStorage.setItem('lastView',e);
+  }
 
   const eventStyleGetter = ( event, start, end, isSelected ) => {
     //console.log(event, start, end, isSelected)
@@ -47,9 +74,18 @@ export const CalendarScreen = () => {
                 startAccessor="start"
                 endAccessor="end"
                 //style={{ height: 500 }}
+                onDoubleClickEvent={ onDoubleClick }
+                onSelectEvent={ onSelectedEvent }
+                onView={ onViewChange }
+                view={ lastView }
                 messages={ messages }
                 eventPropGetter={ eventStyleGetter }
+                components={{
+                  event: CalendarEvent
+                }}
               />
+
+        <CalendarModal />
 
       </div>
   )
