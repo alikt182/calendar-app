11a4106e29b1650cb91a0ca570817ed1181870a9
Ali Monserrate <ali.monserrate.icg@gmail.com>
2022-03-01 14:04:24 -0400

    Conexión con la base de datos y autenticación de usuario completa

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -1,18 +1,69 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import Swal from 'sweetalert2';
+import { useForm } from '../../hooks/useForm';
+import { startLogin, startRegister } from '../actions/auth';
 import './login.css';
 
 export const LoginScreen = () => {
+
+    const dispatch = useDispatch();
+   
+    //Login
+    const [ formLoginValues, handleLoginInputChange ] = useForm({
+        lEmail: 'ali.monserrate.icg@gmail.com',
+        lPassword: '123456'
+    });
+    
+    //Register
+    const [ formRegisterValues, handleRegisterInputChange ] = useForm({
+        rName: 'Nano',
+        rEmail: 'nando@gmail.com',
+        rPassword1: '123456',
+        rPassword2: '123456'
+    });
+
+    //Login
+    const { lEmail, lPassword } = formLoginValues;
+    
+    //Register
+    const { rName, rEmail, rPassword1, rPassword2 } = formRegisterValues;
+
+    //Login
+    const handleLogin = (e) => {
+        e.preventDefault();
+
+        //console.log( formLoginValues );
+        dispatch( startLogin( lEmail,lPassword ));
+
+    }
+
+    //Register
+    const handleRegister = (e) => {
+        e.preventDefault();
+
+        if ( rPassword1 !== rPassword2 ) {
+            return Swal.fire('Error','Las contraseñas no coinciden', 'error');
+        }
+
+        //console.log( formRegisterValues );
+        dispatch( startRegister( rEmail, rPassword1, rName ));
+    }
+
     return (
         <div className="container login-container">
             <div className="row">
                 <div className="col-md-6 login-form-1">
                     <h3>Ingreso</h3>
-                    <form>
+                    <form onSubmit={ handleLogin }>
                         <div className="form-group">
                             <input 
                                 type="text"
                                 className="form-control"
                                 placeholder="Correo"
+                                name="lEmail"
+                                value={ lEmail }
+                                onChange={ handleLoginInputChange }
                             />
                         </div>
                         <div className="form-group">
@@ -20,6 +71,9 @@ export const LoginScreen = () => {
                                 type="password"
                                 className="form-control"
                                 placeholder="Contraseña"
+                                name="lPassword"
+                                value={ lPassword }
+                                onChange={ handleLoginInputChange }
                             />
                         </div>
                         <div className="form-group">
@@ -34,12 +88,15 @@ export const LoginScreen = () => {
 
                 <div className="col-md-6 login-form-2">
                     <h3>Registro</h3>
-                    <form>
+                    <form onSubmit={ handleRegister }>
                         <div className="form-group">
                             <input
                                 type="text"
                                 className="form-control"
                                 placeholder="Nombre"
+                                name="rName"
+                                value={ rName }
+                                onChange={ handleRegisterInputChange }
                             />
                         </div>
                         <div className="form-group">
@@ -47,6 +104,9 @@ export const LoginScreen = () => {
                                 type="email"
                                 className="form-control"
                                 placeholder="Correo"
+                                name="rEmail"
+                                value={ rEmail }
+                                onChange={ handleRegisterInputChange }
                             />
                         </div>
                         <div className="form-group">
@@ -54,6 +114,9 @@ export const LoginScreen = () => {
                                 type="password"
                                 className="form-control"
                                 placeholder="Contraseña" 
+                                name="rPassword1"
+                                value={ rPassword1 }
+                                onChange={ handleRegisterInputChange }
                             />
                         </div>
 
@@ -62,6 +125,9 @@ export const LoginScreen = () => {
                                 type="password"
                                 className="form-control"
                                 placeholder="Repita la contraseña" 
+                                name="rPassword2"
+                                value={ rPassword2 }
+                                onChange={ handleRegisterInputChange }
                             />
                         </div>
 
--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -1,22 +1,78 @@
-import React from 'react';
-import { Navigate, Route, BrowserRouter as Router, Routes } from 'react-router-dom';
-import { LoginScreen } from '../components/auth/LoginScreen';
-import { CalendarScreen } from '../components/calendar/CalendarScreen';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  Route,
+  BrowserRouter as Router,
+  Routes,
+} from "react-router-dom";
+import Swal from "sweetalert2";
+import { startChecking } from "../components/actions/auth";
+import { LoginScreen } from "../components/auth/LoginScreen";
+import { CalendarScreen } from "../components/calendar/CalendarScreen";
+import { PrivateRoute } from "./PrivateRouter";
+import { PublicRoute } from "./PublicRouter";
 
 export const AppRouter = () => {
-  return( 
-        <Router>
-            <div>
-                <Routes>
-                    
-                    <Route exact path='/login' element={ <LoginScreen /> } />
+  const dispatch = useDispatch();
+  const { checking, uid } = useSelector( state => state.auth )
 
-                    <Route path='/' element={ <CalendarScreen /> } />
+  useEffect(() => {
 
-                    <Route path='*' element={<Navigate replace to='/' /> } />
+    dispatch( startChecking() );
 
-                </Routes>
-            </div>
-        </Router>
-    )
+  }, [ dispatch ]);
+
+  if( checking ) {
+
+    //return ( <h5> Espere... </h5> )
+    Swal.fire({
+        title: 'Cargando...',
+        html: 'Espere por favor...',
+        allowEscapeKey: false,
+        allowOutsideClick: false,
+        didOpen: () => {
+          Swal.showLoading()
+        }
+    });
+} else {
+
+    Swal.close();
+
+}
+
+  return (
+    <Router>
+      <div>
+        <Routes>
+          {/* <Route exact path="/login" element={<LoginScreen />} />
+
+          <Route path="/" element={<CalendarScreen />} />
+
+          <Route path="*" element={<Navigate replace to="/" />} /> */}
+
+          <Route exact path='/login' element={ 
+
+              <PublicRoute uid={ uid } >
+
+                <LoginScreen/> 
+
+              </PublicRoute>
+            } 
+          />
+
+          <Route exact path='/*' 
+          
+            element={ 
+              <PrivateRoute uid={ uid }>
+
+                <CalendarScreen />
+
+              </PrivateRoute> 
+            }  
+          />
+
+        </Routes>
+      </div>
+    </Router>
+  );
 };
